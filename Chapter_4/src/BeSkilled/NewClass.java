9ab97c5e54e73075b091c434f9e76dc4185f0e53
mostafiz9900 <mostafiz9900@gmail.com>
2018-09-09 12:57:25 +0600

    java chapter3

---
+++ Chapter_4/src/BeSkilled/NewClass.java
@@ -0,0 +1,22 @@
+package BeSkilled;
+
+import java.util.Scanner;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+       
+        String s= 'welcome to java';
+
+        System.out.println(s.charAt(s.leng  ));
+//        for (int i = 0; i < 35; i++) {
+//            System.out.println(ch);
+//        }
+//        Scanner input = new Scanner(System.in);
+//        String dd = input.nextLine();
+//        String dd2 = input.nextLine();
+//        String dd3 = input.nextLine();
+//        System.out.println(dd.concat(dd2).concat(dd3));
+
+    }
+}
