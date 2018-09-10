17b60e9aa959c114c70a4a8cff039a1ba0723064
mostafiz9900 <mostafiz9900@gmail.com>
2018-09-10 13:04:18 +0600

    exam sql & java ch_4

---
+++ Chapter_4/src/BeSkilled/ComparingStrings_133.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BeSkilled;
+
+/**
+ *
+ * @author User
+ */
+public class ComparingStrings_133 {
+    public static void main(String[] args) {
+        String str1="Bangladesh";
+         String str2="India";
+         if (str1==str2){
+             System.out.println("True");
+         }else{
+             System.out.println("False");
+         }
+    //two way
+    if(str1.equals(str2)){
+        System.out.println("True");
+         }else{
+             System.out.println("False");
+         }
+   
+         
+        
+    }
+}
---
+++ Chapter_4/src/BeSkilled/OrderTwoCities_135.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BeSkilled;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class OrderTwoCities_135 {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enter the first city");
+        String city1 = input.toString();
+        System.out.println("Enter the seond city");
+         if(city1.compareTo(city2)<0){
+             System.out.println("");}
+    }
+}
