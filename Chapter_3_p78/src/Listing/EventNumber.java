1e0e5df2c87ad2ea70274e4f1c1e595f25d2fbe8
Your Name <you@example.com>
2018-09-11 23:58:59 +0600

    remove sql

--- Chapter_3_p78/src/Listing/EventNumber.java
+++ Chapter_3_p78/src/Listing/EventNumber.java
@@ -1,16 +1,17 @@
-
 package Listing;
 
 import java.util.Scanner;
 
 public class EventNumber {
+
     public static void main(String[] args) {
-        Scanner inputNumber = new Scanner (System.in);
+        Scanner inputNumber = new Scanner(System.in);
         System.out.println("Enter Number");
         int num = inputNumber.nextInt();
-         if (num % 2 == 0){
-             System.out.println("Event Number " + num);
-         }else {
-             System.out.println("Old Number " + num);}
+        if (num % 2 == 0) {
+            System.out.println("Event Number " + num);
+        } else {
+            System.out.println("Old Number " + num);
+        }
     }
 }
