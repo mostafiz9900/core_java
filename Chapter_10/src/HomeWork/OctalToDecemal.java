136f97f4b4aea18dae838fd58f6af1597310d606
Your Name <you@example.com>
2018-09-23 01:01:05 +0600

    Cheek point

---
+++ Chapter_10/src/HomeWork/OctalToDecemal.java
@@ -0,0 +1,11 @@
+package HomeWork;
+
+public class OctalToDecemal {
+
+    public static void main(String[] args) {
+        //0 manta dara octa vujay
+        int i = 010;
+        int j = 07;
+        System.out.println(i + " v+ " + j);
+    }
+}
